6c3c8c15158c00efb0281fbe85ee05feee9bf4ff
Shrenik Jain <shrenikkjain81@gmail.com>
2024-03-08 12:39:31 +0530

    add new solutions

---
+++ Arrays & Hashing/contains_duplicate.py
@@ -0,0 +1,30 @@
+'''
+Question: https://leetcode.com/problems/contains-duplicate/description/
+'''
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        '''
+        create a dictionary to store only unique numbers. If number occurs again, it contains duplicate
+        '''
+        # create a dictionary to store unique numbers as keys
+        count_dict = {}
+
+        for num in nums:
+            # if number already in dictionary, return True
+            if num in count_dict:
+                return True
+            # if number occurs first time, store it in dictionary
+            else:
+                count_dict[num] = 1
+
+        return False
+    
+        
+        # Another Solution
+        '''
+        create a set and check its length with original list, if different then contains duplicate
+        since set only stores unique values
+        '''
+        return len(nums) == len(set(nums))
+        
---
+++ QUESTIONS.md
@@ -0,0 +1,5 @@
+## [Arrays & Hashing](Arrays%20&%20Hashing)
+
+| # | Question | Solution |
+|---| -------- | -------- | 
+| 1 | [Contains Duplicate](https://leetcode.com/problems/contains-duplicate/) | [contains_duplicate.py](Arrays%20&%20Hashing/contains_duplicate.py) |
